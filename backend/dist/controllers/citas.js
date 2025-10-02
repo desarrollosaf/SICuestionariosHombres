b98a58b1daa1d350475b1fb68371268d40646ed4
Nahum <flashjzd58@gmail.com>
2025-10-02 14:44:40 -0600

    cambio controlado de citas

--- backend/dist/controllers/citas.js
+++ backend/dist/controllers/citas.js
@@ -24,7 +24,10 @@ const t_direccion_1 = __importDefault(require("../models/saf/t_direccion"));
 const t_departamento_1 = __importDefault(require("../models/saf/t_departamento"));
 const dp_fum_datos_generales_1 = require("../models/fun/dp_fum_datos_generales");
 const dp_datospersonales_1 = require("../models/fun/dp_datospersonales");
-const fun_1 = __importDefault(require("../database/fun")); // La conexión
+const fun_1 = __importDefault(require("../database/fun"));
+const pdfkit_1 = __importDefault(require("pdfkit"));
+const fs_1 = __importDefault(require("fs"));
+const path_1 = __importDefault(require("path"));
 dp_datospersonales_1.dp_datospersonales.initModel(fun_1.default);
 dp_fum_datos_generales_1.dp_fum_datos_generales.initModel(fun_1.default);
 const getHorariosDisponibles = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
@@ -64,6 +67,7 @@ const getHorariosDisponibles = (req, res) => __awaiter(void 0, void 0, void 0, f
 });
 exports.getHorariosDisponibles = getHorariosDisponibles;
 const savecita = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    var _a;
     try {
         const { body } = req;
         const limite = 3;
@@ -97,6 +101,50 @@ const savecita = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
             correo: body.correo,
             telefono: body.telefono,
         });
+        const horarios = yield horarios_citas_1.default.findOne({
+            where: { id: body.horario_id }
+        });
+        const horario = horarios ? `${horarios.horario_inicio} - ${horarios.horario_fin}` : '';
+        const sede2 = ((_a = (yield sedes_1.default.findOne({ where: { id: body.sede_id } }))) === null || _a === void 0 ? void 0 : _a.sede) || "";
+        const Validacion = yield dp_fum_datos_generales_1.dp_fum_datos_generales.findOne({
+            where: { f_rfc: body.rfc },
+            attributes: ["f_nombre", "f_primer_apellido", "f_segundo_apellido", "f_sexo", "f_fecha_nacimiento"]
+        });
+        if (!Validacion) {
+            throw new Error("No se encontró información para el RFC proporcionado");
+        }
+        const nombreCompleto = [
+            Validacion.f_nombre,
+            Validacion.f_primer_apellido,
+            Validacion.f_segundo_apellido
+        ].filter(Boolean).join(" ");
+        const sexo = Validacion.f_sexo || "";
+        let edad = "";
+        if (Validacion.f_fecha_nacimiento) {
+            const nacimiento = new Date(Validacion.f_fecha_nacimiento);
+            const hoy = new Date();
+            edad = (hoy.getFullYear() - nacimiento.getFullYear()).toString();
+            const mes = hoy.getMonth() - nacimiento.getMonth();
+            if (mes < 0 || (mes === 0 && hoy.getDate() < nacimiento.getDate())) {
+                edad = (parseInt(edad) - 1).toString();
+            }
+        }
+        const pdfBuffer = yield generarPDFBuffer({
+            folio: cita.id.toString(),
+            nombreCompleto: nombreCompleto,
+            sexo: sexo,
+            edad: edad,
+            correo: body.correo,
+            curp: body.rfc,
+            fecha: new Date().toLocaleDateString(),
+            telefono: body.telefono,
+            sede: sede2,
+            horario: horario
+        });
+        // Enviar el PDF como respuesta al usuario
+        res.setHeader("Content-Type", "application/pdf");
+        res.setHeader("Content-Disposition", `attachment; filename="Cita-${body.fecha_cita}.pdf"`);
+        res.send(pdfBuffer);
         return res.json({
             status: 200,
             msg: "Cita registrada correctamente",
@@ -299,3 +347,62 @@ const getcitasFecha = (req, res) => __awaiter(void 0, void 0, void 0, function* 
     }
 });
 exports.getcitasFecha = getcitasFecha;
+function generarPDFBuffer(data) {
+    return new Promise((resolve, reject) => __awaiter(this, void 0, void 0, function* () {
+        const doc = new pdfkit_1.default({ size: 'LETTER', margin: 50 });
+        const chunks = [];
+        // Ruta donde se guardará el PDF
+        const pdfDir = path_1.default.join(process.cwd(), 'public/pdfs');
+        if (!fs_1.default.existsSync(pdfDir)) {
+            fs_1.default.mkdirSync(pdfDir, { recursive: true });
+        }
+        const fileName = `acuse_${data.folio}.pdf`;
+        const filePath = path_1.default.join(pdfDir, fileName);
+        const writeStream = fs_1.default.createWriteStream(filePath);
+        doc.pipe(writeStream);
+        doc.on('data', (chunk) => chunks.push(chunk));
+        doc.on('end', () => resolve(Buffer.concat(chunks)));
+        doc.on('error', reject);
+        // ✅ Imagen de fondo
+        doc.image(path_1.default.join(__dirname, '../assets/acusederegistro.png'), 0, 0, {
+            width: doc.page.width,
+            height: doc.page.height,
+        });
+        // Título centralizado
+        doc.moveDown(4);
+        doc.fontSize(18).font('Helvetica-Bold').text('CAMPANA GRATUITA DE SALUD MASCULINA', {
+            align: 'center',
+            underline: true
+        });
+        // Datos del paciente
+        doc.moveDown(2);
+        doc.fontSize(12)
+            .font('Helvetica')
+            .text(`Paciente: ${data.nombreCompleto} | ${data.sexo} | ${data.edad}`, { align: 'left' })
+            .text(`CURP: ${data.curp}`, { align: 'left' })
+            .text(`Correo electrónico: ${data.correo} | Teléfono: ${data.telefono} `, { align: 'left' })
+            .text(`Ubicación: ${data.sede}`, { align: 'left' })
+            .text(`Horario: ${data.horario}`, { align: 'left' });
+        doc.moveDown();
+        // Información adicional de la campaña
+        doc.fontSize(11).text('El Voluntariado del Poder Legislativo del Estado de México organiza la Campaña gratuita de salud masculina, que incluye chequeo médico y la prueba de Antígeno Prostático Específico (PSA).', { align: 'justify' });
+        // Detalles de la documentación
+        doc.fontSize(11).text('Para acceder a este beneficio, es indispensable presentar en el día y hora asignados la siguiente documentación:', { align: 'justify' });
+        doc.moveDown();
+        doc.fontSize(11).list([
+            'Identificación oficial: Se aceptará únicamente credencial para votar (INE) vigente o gafete oficial expedido por la Dirección de Administración y Desarrollo de Personal. Deberá presentar en original y copia. Si no se presenta alguno de estos documentos el día de la cita, no podrá realizar su examen y se le dará por perdido.',
+        ], { bulletIndent: 20 });
+        doc.moveDown();
+        // Pie de página con aviso de privacidad
+        doc.font('Helvetica-Bold')
+            .fontSize(10)
+            .text('Aviso de Privacidad', { align: 'left' });
+        doc.font('Helvetica') // volver a fuente normal
+            .fontSize(9)
+            .text('Consúltalo en:', { align: 'left' });
+        doc.font('Helvetica')
+            .fontSize(9)
+            .text('https://legislacion.legislativoedomex.gob.mx/storage/documentos/avisosprivacidad/expediente-clinico.pdf', { align: 'left' });
+        doc.end();
+    }));
+}
